a70825cbffecd8928b9dd49cdafa66ca948e6dee
chenbojian <chenbojian@foxmail.com>
2021-07-27 00:12:57 +0800

    update style

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,13 @@ function App() {
   return (
     <div className="App">
       <div className='task-list'>
-        <div className='task-item'>
-          <Input value="task1"/>
-          <Button type='primary'>开始</Button>
-        </div>
-        <div className='task-item'>
-          <Input value="task2"/>
-          <Button type='primary'>开始</Button>
-        </div>
-        <div className='task-item'>
-          <Input value="task3"/>
-          <Button type='primary'>开始</Button>
-        </div>
+        {Array(20).fill(1).map((_, i) => (
+          <div className='task-item'>
+            <div>task{i}</div>
+          </div>
+        ))}
       </div>
-      <div className='btn-container'>
-          <Button type='primary' shape='circle' className='start-btn-large'>开始</Button>
-      </div>
+      <Button type='default' className='start-btn shadow' shape='circle'>开始</Button>
     </div>
   );
 }
